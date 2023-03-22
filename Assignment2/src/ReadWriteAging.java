36e9996c7f76c9c8fe94394fca08157d481cdde5
mdeliso97 <marco.deliso@students.unibe.ch>
2023-03-22 22:23:07 +0100

    Code still does not behave as intended, need to further check what problems may happen

--- Assignment2/src/ReadWriteAging.java
+++ Assignment2/src/ReadWriteAging.java
@@ -1,16 +1,13 @@
 
 public class ReadWriteAging {
     private int readers;
-    private int writers;
     private int counter;
     private boolean isWriter;
     // implements aging for writers to avoid starvation
     private int writeWaitTime;
-    private int readWaitTime;
-    private int lastWriterId;
 
     public synchronized void lockRead(int id) throws InterruptedException {
-        while (isWriter || (readers > 0 && writeWaitTime < readers) || (id != lastWriterId && writeWaitTime > 0)) {
+        while (isWriter || (readers > 0 && writeWaitTime < readers)) {
             if (readers > 0 && writeWaitTime < readers) {
                 System.out.println("I'm reader " + id + " and I've been greedy, I'll let writer go first");
             }
@@ -22,30 +19,22 @@ public class ReadWriteAging {
 
     public synchronized void unlockRead() {
         readers--;
-        writeWaitTime += writers;
-        readWaitTime = 0;
+        writeWaitTime++;
         notifyAll();
     }
 
-    public synchronized void lockWrite(int id) throws InterruptedException {
-        while (isWriter || readers > 0 || (writers > 0 && readWaitTime < writers) || (id != lastWriterId && readWaitTime > 0)) {
-            if (writers > 0 && writeWaitTime < writers) {
-                System.out.println("I'm writer " + id + " and I've been greedy, I'll let reader go first");
-            }
+    public synchronized void lockWrite() throws InterruptedException {
+        while (isWriter || readers > 0) {
             wait();
         }
         isWriter = true;
-        writers++;
     }
 
     public synchronized void unlockWrite(int id) {
         counter = id;
         System.out.println("I'm writer " + id + " and I wrote counter = " + id);
         isWriter = false;
-        lastWriterId = id;
-        readWaitTime += readers;
         writeWaitTime = 0;
-        writers--;
         notifyAll();
     }
 
@@ -73,7 +62,7 @@ public class ReadWriteAging {
             final int id = i;
             Thread writerThread = new Thread(() -> {
                 try {
-                    lock.lockWrite(id);
+                    lock.lockWrite();
                     // perform write operation
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
