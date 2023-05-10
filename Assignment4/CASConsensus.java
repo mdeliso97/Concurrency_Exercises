3b117eeb15a58704c5db2ce4a2e6b91c303f973c
mdeliso97 <marco.deliso@students.unibe.ch>
2023-05-10 23:01:56 +0200

    Some new implementation

--- Assignment4/CASConsensus.java
+++ Assignment4/CASConsensus.java
@@ -1,85 +1,123 @@
 import java.util.Random;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class CASConsensus extends Thread implements IConsensus {
+public class CASConsensus implements IConsensus {
 
-    private volatile Object decision = null;
-    static Semaphore mutex = new Semaphore(1);
-    Random random = new Random();
+    private final AtomicReference<Object> decision = new AtomicReference<>(null);
+    private final int numThreads;
+    private final Random random = new Random();
+    private final Object[] values;
 
-    public void run() {
-        int rand = random.nextInt(5) + 1;
-        Object result;
-        try {
-            mutex.acquire();
-            result = decide(rand);
-            mutex.release();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    public CASConsensus(int numThreads) {
+        this.numThreads = numThreads;
+        this.values = new Object[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            this.values[i] = new Object();
         }
-        System.out.println("Thread " + Thread.currentThread().getId() + " decided on " + result);
     }
 
-    public Object getDecision() {
-
-        return decision;
-    }
-
-    @Override
-    public Object decide(Object v) throws InterruptedException {
+    public Object decide(Object value) {
         while (true) {
-            Object curDecision = getDecision();
-            if (curDecision != null) {
-                //System.out.println("The decision made is " + curDecision);
-                return curDecision;
+            Object currentDecision = decision.get();
+            if (currentDecision != null) {
+                return currentDecision;
             }
-            if (compareAndSetDecision(null, v)) {
-                //System.out.println("The decision made is " + v);
-                return v;
+
+            int numAgree = 1;
+            values[0] = value;
+
+            // Propose values from other threads
+            for (int i = 0; i < numThreads; i++) {
+                if (i == ThreadId.get()) continue;
+                values[numAgree] = proposeValue(i);
+                if (values[numAgree] != null) numAgree++;
+            }
+
+            // Check if consensus is reached
+            if (numAgree == numThreads) {
+                Object agreedValue = findMajority(values);
+                if (decision.compareAndSet(null, agreedValue)) {
+                    return agreedValue;
+                }
             }
         }
     }
 
-    private boolean compareAndSetDecision(Object expect, Object update) {
-        synchronized (this) {
-            if (decision == expect) {
-                decision = update;
-                return true;
-            }
-            return false;
+    private Object proposeValue(int threadID) {
+        // Simulate a delay
+        try {
+            Thread.sleep(random.nextInt(5) + 1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        // Propose a value
+        return values[threadID];
     }
 
+    private Object findMajority(Object[] values) {
+        int majorityCount = 0;
+        Object majorityValue = null;
+        for (Object value : values) {
+            if (value == null) continue;
+            int count = 0;
+            for (Object otherValue : values) {
+                if (otherValue == null) continue;
+                if (value.equals(otherValue)) count++;
+            }
+            if (count > majorityCount) {
+                majorityCount = count;
+                majorityValue = value;
+            }
+        }
+        return majorityValue;
+    }
     public static void main(String[] args) {
 
-        //check that inputs is 1
+        // Check that input is positive
         if (args.length != 1) {
-            System.out.println("The argument provided is not 1!");
+            System.out.println("Usage: java CASConsensus <numThreads>");
             return;
         }
-
-        //parse input arguments
-        int T = Integer.parseInt(args[0]);
-
-        //check that inputs are positive
-        if (T <= 0) {
+        int numThreads = Integer.parseInt(args[0]);
+        if (numThreads <= 0) {
             System.out.println("The integer you entered is not positive, please try again");
             return;
         }
 
-        Thread[] threads = new Thread[T];
-
-        for (int i = 0; i < T; i++) {
-            threads[i] = new Thread(new CASConsensus());
+        // Create threads and start them
+        ThreadId.reset();
+        CASConsensus consensus = new CASConsensus(numThreads);
+        Thread[] threads = new Thread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new Thread(new ConsensusThread(consensus));
             threads[i].start();
         }
 
-        // Wait for threads completion
-        for (int i = 0; i < T; i++) {
+        // Wait for threads to complete
+        for (int i = 0; i < numThreads; i++) {
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
+
+        // Print final decision
+        System.out.println("Consensus reached: " + consensus.decision.get());
     }
 }
+
+class ThreadId {
+    private static int nextID = 0;
+    private static ThreadLocal<Integer> threadId = ThreadLocal.withInitial(() -> nextID++);
+
+    public static int get() {
+        return threadId.get();
+    }
+
+    public static void reset() {
+        nextID = 0;
+    }
+}
+
