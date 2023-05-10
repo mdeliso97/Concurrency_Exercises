953b08261842b14e58e092a1c60e1bb8fba17000
mdeliso97 <marco.deliso@students.unibe.ch>
2023-05-10 12:16:15 +0200

    Fixed partially, need to add random function

--- Assignment4/CASConsensus.java
+++ Assignment4/CASConsensus.java
@@ -1,29 +1,23 @@
-public class CASConsensus implements IConsensus, Runnable {
+public class CASConsensus extends Thread implements IConsensus {
 
     private volatile Object decision = null;
 
-    public class CASConsensusThread implements Runnable {
-        private CASConsensus casConsensus;
-
-        public CASConsensusThread() {
-            this.casConsensus = new CASConsensus();
-        }
-
-
-        public void run() {
-
-        }
+    public void run() {
+        // Generate random value function
+        Object result = decide(null);
+        System.out.println("Thread " + Thread.currentThread().getId() + " decided on " + result);
     }
+
     @Override
     public Object decide(Object v) {
         while (true) {
             Object curDecision = decision;
             if (curDecision != null) {
-                System.out.println("The decision made is " + curDecision);
+                //System.out.println("The decision made is " + curDecision);
                 return curDecision;
             }
             if (compareAndSetDecision(null, v)) {
-                System.out.println("The decision made is " + v);
+                //System.out.println("The decision made is " + v);
                 return v;
             }
         }
@@ -43,7 +37,7 @@ public class CASConsensus implements IConsensus, Runnable {
 
         //check that inputs is 1
         if (args.length != 1) {
-            System.out.println("The arguments provided are not 2!");
+            System.out.println("The argument provided is not 1!");
             return;
         }
 
@@ -52,7 +46,7 @@ public class CASConsensus implements IConsensus, Runnable {
 
         //check that inputs are positive
         if (T <= 0) {
-            System.out.println("The integers you entered are not positive, please try again");
+            System.out.println("The integer you entered is not positive, please try again");
             return;
         }
 
